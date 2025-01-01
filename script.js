bddc8c6349b7f86767646e5edf56fb130c735ad8
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-01-01 22:56:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+const student = {name:"Monkey.D.Luffy"};
+
+Object.prototype.getKeys() = function(){
+	return Object.keys(this);
+}
+
