45335ede4fd69cc697585e9d6952554ebfb0066b
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-01-01 23:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //your JS code here. If required
 
-const student = {name:"Monkey.D.Luffy"};
+const student = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"
+};
 
 Object.prototype.getKeys() = function(){
 	return Object.keys(this);
 }
 
+
+
+// Add the getKeys method to the Object prototype
+Object.prototype.getKeys = function () {
+    return Object.keys(this);
+};
